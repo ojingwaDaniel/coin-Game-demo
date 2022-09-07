6e0b927edff48e09256854dd5b962f0c22534d10
ojingwaDaniel <ojingwadanny@gmail.com>
2022-09-07 16:27:06 +0100

    modified the file

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ function isTouching(a, b) {
     aRect.left > bRect.left + bRect.width
   );
 }
+let player = document.querySelector('#player')
+window.addEventListener('keydown',function(evt){
+  if (evt.key === 'ArrowDown') {
+    let top = position(player.style.top);
+    player.style.top = `${top + 50}px`;
+  } else if(evt.key==='ArrowUp'){ 
+    let top = position(player.style.top);
+    player.style.top = `${top -50}px`;
+  }
+   
+   
+  
+})
+const moveVertical = (side,number)=>{
+  
+}
+const moveHorizontal = (side, number) => {
+
+};
+const position  = (digit)=>{
+  if(!digit) return 100
+    return parseInt(digit.slice(0,-2))
+}
