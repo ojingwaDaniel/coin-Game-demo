2dacb5289235689f06f31cdc22a4fb6f7048699e
ojingwaDaniel <ojingwadanny@gmail.com>
2022-09-07 23:11:45 +0100

    cleaned up the file

--- script.js
+++ script.js
@@ -12,19 +12,16 @@ function isTouching(a, b) {
 let player = document.querySelector('#player')
 window.addEventListener('keydown',function(evt){
   if (evt.key === 'ArrowDown') {
-    let top = position(player.style.top);
-    player.style.top = `${top + 50}px`;
+   moveVertical(player,50)
   } else if(evt.key==='ArrowUp'){ 
-    let top = position(player.style.top);
-    player.style.top = `${top -50}px`;
+    moveVertical(player,-50)
+   
   } else if(evt.key === 'ArrowRight'){
-    let left = position(player.style.left)
-    player.style.left = `${left + 50}px`
+    moveHorizontal(player,50)
     player.style.transform = "scale(1,1)";
 
   } else if(evt.key === 'ArrowLeft'){
-    let left = position(player.style.left)
-    player.style.left = `${left - 50}px`
+    moveHorizontal(player,-50)
     player.style.transform = 'scale(-1,1)'
   }
    
@@ -32,9 +29,13 @@ window.addEventListener('keydown',function(evt){
   
 })
 const moveVertical = (side,number)=>{
+  let top = position(side.style.top);
+  side.style.top = `${top + number}px`;
   
 }
 const moveHorizontal = (side, number) => {
+  let left = position(side.style.left);
+  side.style.left = `${left + number}px`;
 
 };
 const position  = (digit)=>{
