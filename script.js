5a886b2ad3fae5236b7ebb078cabce7fb149f82a
ojingwaDaniel <ojingwadanny@gmail.com>
2022-09-08 12:46:13 +0100

    added a scoreboard

--- script.js
+++ script.js
@@ -10,6 +10,24 @@ function isTouching(a, b) {
   );
 }
 let player = document.querySelector('#player')
+let coin = document.querySelector('#coin')
+let scoreboard = document.querySelector('span')
+let score = 0
+scoreboard.innerHTML = `score : ${score}`
+scoreboard.classList.add('dashboard')
+document.body.prepend(scoreboard)
+const moveVertical = (side, number) => {
+  let top = position(side.style.top);
+  side.style.top = `${top + number}px`;
+};
+const moveHorizontal = (side, number) => {
+  let left = position(side.style.left);
+  side.style.left = `${left + number}px`;
+};
+const position = (digit) => {
+  if (!digit) return 100;
+  return parseInt(digit.slice(0, -2));
+};
 window.addEventListener('keydown',function(evt){
   if (evt.key === 'ArrowDown') {
    moveVertical(player,50)
@@ -24,21 +42,19 @@ window.addEventListener('keydown',function(evt){
     moveHorizontal(player,-50)
     player.style.transform = 'scale(-1,1)'
   }
+  if (isTouching(player,coin)){
+  positionCoin()
+  score++
+  scoreboard.innerHTML = `Score : ${score}`
+}
    
    
   
 })
-const moveVertical = (side,number)=>{
-  let top = position(side.style.top);
-  side.style.top = `${top + number}px`;
-  
-}
-const moveHorizontal = (side, number) => {
-  let left = position(side.style.left);
-  side.style.left = `${left + number}px`;
-
+let positionCoin = () => {
+  let y = Math.floor(Math.random() * innerHeight);
+  let x = Math.floor(Math.random() * innerWidth);
+  coin.style.top = `${y}px`;
+  coin.style.left = `${x}px`;
 };
-const position  = (digit)=>{
-  if(!digit) return 100
-    return parseInt(digit.slice(0,-2))
-}
+positionCoin()
